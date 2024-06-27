c60253d0a903374bde406433b7ab31aaa0df188b
Johannes Hornbrinck <johanneshornbrinck@gmail.com>
2024-06-27 18:09:33 +0200

    feat: update

---
+++ find.go
@@ -0,0 +1,173 @@
+package main
+
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
+
+	"github.com/charmbracelet/log"
+)
+
+type Result interface {
+	unwrap() (Result, error)
+}
+
+type SearchWorkList struct {
+	WorkList
+	Results *chan Result
+}
+
+//
+// type Results interface {
+// 	Get() ([]Result, error)
+// }
+
+type FileContentResult struct {
+	path string
+	line int
+	text string
+}
+
+func (r *FileContentResult) unwrap() (Result, error) {
+	return r, nil
+}
+
+func NewFilecontentResult(text string, line int, path string) *FileContentResult {
+	return &FileContentResult{text: text, line: line, path: path}
+}
+
+type SearchFileContentJob struct {
+	path    string
+	find    string
+	results chan<- Result
+}
+
+func newFileContentSearch(path, find string, results chan<- Result) *SearchFileContentJob {
+	return &SearchFileContentJob{path: path, find: find, results: results}
+}
+
+// Do method for InFileJob to perform the search
+func (job *SearchFileContentJob) Do() error {
+	file, err := os.Open(job.path)
+	if err != nil {
+		log.Printf("Error opening file %s: %v\n", job.path, err)
+		return err
+	}
+	defer func() {
+		if r := recover(); r != nil {
+			log.Printf("Recovered in Do: %v", r)
+		}
+	}()
+
+	defer func(file *os.File) {
+		closErr := file.Close()
+		if closErr != nil {
+			log.Errorf("Error closing file %s: %v\n", job.path, closErr)
+		}
+	}(file)
+
+	scanner := bufio.NewScanner(file)
+	lineNum := 1
+	for scanner.Scan() {
+		if strings.Contains(scanner.Text(), job.find) {
+			result := NewFilecontentResult(scanner.Text(), lineNum, job.path)
+			job.results <- result
+		}
+		lineNum++
+	}
+	return nil
+}
+
+func numberOfFilesInDir(path string) int {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Println("ReadDir error:", err)
+		return 0
+	}
+	return len(entries)
+}
+
+func numberOfFilesInDirRecursive(path string) int {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Println("ReadDir error:", err)
+		return 0
+	}
+	count := 0
+	for _, entry := range entries {
+		if entry.IsDir() {
+			count += numberOfFilesInDirRecursive(filepath.Join(path, entry.Name()))
+		} else {
+			count++
+		}
+	}
+	return count
+}
+
+func addFileEntryToJob(entry os.DirEntry, wl *WorkList, results chan<- Result, path, find string) {
+	path = filepath.Join(path, entry.Name())
+	if entry.IsDir() {
+		discoverFileContentSearchDirs(wl, path, find, results)
+	} else {
+		job := newFileContentSearch(path, find, results)
+		wl.Add(job)
+	}
+}
+
+func discoverFileContentSearchDirs(wl *WorkList, path string, find string, results chan<- Result) {
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		fmt.Println("ReadDir error:", err)
+		return
+	}
+	for _, entry := range entries {
+		addFileEntryToJob(entry, wl, results, path, find)
+	}
+}
+
+func SearchFileContent(searchTerm, searchDir string) <-chan Result {
+	var wg sync.WaitGroup
+
+	numWorkers := numberOfFilesInDirRecursive(searchDir)
+
+	log.Info("Number of files to search:", numWorkers)
+
+	results := make(chan Result, numWorkers) // Adjust buffer size as necessary
+	wl := NewWorkList(numWorkers)
+
+	workerFunc := func() {
+		defer wg.Done()
+		for job := range wl.Jobs() { // Assuming Jobs() provides a channel of Job interface
+			err := job.Do()
+			var jobsDoneErr *WorkIsDoneError
+			if errors.As(err, &jobsDoneErr) {
+				return // work is done
+			} else if err != nil {
+				log.Errorf("Error processing job: %v", err)
+			}
+		}
+	}
+
+	wg.Add(numWorkers + 1)
+
+	go func() {
+		defer wg.Done()
+		discoverFileContentSearchDirs(wl, searchDir, searchTerm, results)
+		wl.Finalize(numWorkers)
+	}()
+
+	for i := 0; i < numWorkers; i++ {
+		go workerFunc()
+	}
+
+	go func() {
+		wg.Wait()
+		close(results)
+	}()
+
+	return results
+}
--- worklist.go
+++ worklist.go
@@ -1,7 +1,6 @@
-package work
+package main
 
 import (
-	"fmt"
 	"sync"
 )
 
@@ -9,45 +8,82 @@ type Job interface {
 	Do() error
 }
 
-// remember to handle panics in the Do method
-//func (job *SampleJob) Do() {
-//	defer func() {
-//		if r := recover(); r != null {
-//			fmt.Println("Recovered in Do", r)
-//		}
-//	}()
-//	// existing code
-//}
+type WorkIsDoneError struct {
+	Message string
+}
 
-type Worklist struct {
+func (w *WorkIsDoneError) Error() string {
+	return w.Message
+}
+
+// finished
+type finshedJob struct {
+	done func() error
+}
+
+func (f *finshedJob) Do() error {
+	return f.done()
+}
+
+func newFinishedJob() *finshedJob {
+	return &finshedJob{
+		done: func() error {
+			return &WorkIsDoneError{"Job is done"}
+		},
+	}
+}
+
+type WorkList struct {
 	jobs      chan Job
 	closed    bool
 	closelock sync.Mutex
 }
 
-// New initializes a new Worklist with a specified capacity for the jobs channel
-func New(capacity int) *Worklist {
-	return &Worklist{
+// New initializes a new WorkList with a specified capacity for the jobs channel
+func NewWorkList(capacity int) *WorkList {
+	return &WorkList{
 		jobs: make(chan Job, capacity),
 	}
 }
 
-func NewWorklist() *Worklist {
-	return &Worklist{jobs: make(chan Job)}
-}
-
-func (w *Worklist) Add(job Job) {
+func (w *WorkList) Add(job Job) {
 	w.jobs <- job
 }
 
-// Next dequeues a job from the worklist, returning false if no jobs are left
-func (w *Worklist) Next() (Job, bool) {
+// Next dequeues a job from the workList, returning false if no jobs are left
+func (w *WorkList) Next() (Job, bool) {
+	w.closelock.Lock()
+	defer w.closelock.Unlock()
+
 	job, ok := <-w.jobs
-	return job, ok
+	if !ok {
+		return nil, false // Channel is closed, no more jobs
+	}
+
+	// If a job signals that work is done, initiate cleanup
+	if _, ok := job.(*finshedJob); ok {
+		if !w.closed {
+			close(w.jobs)
+			w.closed = true
+		}
+		return nil, false // Signal to worker that no more jobs will come
+	}
+
+	return job, true
+}
+
+// We add a `NoMoreJobs` message to the workList for each
+// worker that we are using. Once the worker receives this
+// message, it will terminate. After each worker terminates,
+// the program can continue.
+func (w *WorkList) Finalize(numWorkers int) {
+	for i := 0; i < numWorkers; i++ {
+		w.Add(newFinishedJob())
+	}
 }
 
 // Close safely closes the jobs channel, ensuring no more jobs are added
-func (w *Worklist) Close() {
+func (w *WorkList) Close() {
 	w.closelock.Lock()
 	defer w.closelock.Unlock()
 	if !w.closed {
@@ -56,19 +92,19 @@ func (w *Worklist) Close() {
 	}
 }
 
-func (w *Worklist) Jobs() <-chan Job {
+func (w *WorkList) Jobs() <-chan Job {
 	return w.jobs
 }
 
-func (w *Worklist) Len() int {
+func (w *WorkList) Len() int {
 	return len(w.jobs)
 }
 
-func (w *Worklist) IsEmpty() bool {
+func (w *WorkList) IsEmpty() bool {
 	return w.Len() == 0
 }
 
-func (w *Worklist) IsClosed() bool {
+func (w *WorkList) IsClosed() bool {
 	select {
 	case _, ok := <-w.jobs:
 		return !ok
@@ -77,84 +113,7 @@ func (w *Worklist) IsClosed() bool {
 	}
 }
 
-func (w *Worklist) Wait() {
+func (w *WorkList) Wait() {
 	for !w.IsClosed() {
 	}
-}
-
-func (w *Worklist) DoAll() {
-	for job, ok := w.Next(); ok; job, ok = w.Next() {
-		job.Do()
-	}
-}
-
-func (w *Worklist) DoAllConcurrently(wg *sync.WaitGroup) {
-	for job, ok := w.Next(); ok; job, ok = w.Next() {
-		wg.Add(1)
-		go func(job Job) {
-			defer wg.Done()
-			job.Do()
-		}(job)
-	}
-	wg.Wait() // wait for all goroutines to finish
-}
-
-func ExecuteAll(jobs []Job) {
-	w := New(len(jobs))
-	for _, job := range jobs {
-		w.Add(job)
-	}
-	w.DoAll()
-}
-
-func ExecuteAllConcurrently(jobs []Job) {
-	w := New(len(jobs))
-
-	var wg sync.WaitGroup
-	wg.Add(len(jobs))
-
-	for _, job := range jobs {
-		go func(job Job) {
-			defer wg.Done() // Ensure that wg.Done() is called after job.Do() finishes
-			w.Add(job)
-		}(job)
-	}
-
-	go func() {
-		defer w.Close()
-		w.DoAllConcurrently(&wg)
-	}()
-
-	wg.Wait()
-}
-
-func ExecuteAllConcurrentlyWithProgress(jobs []Job) {
-	w := New(len(jobs))
-	var wg sync.WaitGroup
-	var progressMutex sync.Mutex
-	progress := 0
-
-	wg.Add(len(jobs))
-
-	for _, job := range jobs {
-		go func(job Job) {
-			defer wg.Done() // ensure wg.Done() is called after the job
-			err := job.Do()
-			if err != nil {
-				return
-			} // Execute the job
-			progressMutex.Lock()
-			progress++
-			fmt.Printf("\rProgress: %d%%", (progress*100)/len(jobs))
-			progressMutex.Unlock()
-		}(job)
-	}
-
-	go func() {
-		defer w.Close()
-		wg.Wait()
-		fmt.Println("\nAll jobs completed.")
-	}()
-
-	wg.Wait()
 }
